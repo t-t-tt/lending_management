a324f612483058166933daa8b243beec85b92689
tt-tt-c <tsuruga10415@gmail.com>
2021-06-16 15:34:51 +0900

    編集機能追加

--- src/main/java/taro/controller/EquipController.java
+++ src/main/java/taro/controller/EquipController.java
@@ -13,9 +13,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import taro.entity.EquipEntity;
+import taro.entity.LendEntity;
+import taro.entity.UserEntity;
 import taro.form.EquipForm;
 import taro.service.EquipService;
 import taro.service.LendService;
+import taro.service.UserService;
 
 /**
  * 機器コントローラクラスです.
@@ -28,6 +31,9 @@ public class EquipController {
 
 	@Autowired
 	LendService lendService;
+
+	@Autowired
+	UserService userService;
 
 	@GetMapping("/equip")
 	public String index(Model model) {
@@ -44,11 +50,12 @@ public class EquipController {
 	public String equipList(Model model) {
 		// DBに登録されている機器の一覧を取得
 		List<EquipEntity> equipList = equipService.findByIsDeletedFalse();
-		System.out.println(equipList.size()+"機器一覧");
+		System.out.println(equipList.size() + "機器一覧");
 
 		int letedPcNum = 0;
-		for (EquipEntity equip:equipList) {
-			if(equip.getIsLent()) letedPcNum++;
+		for (EquipEntity equip : equipList) {
+			if (equip.getIsLent())
+				letedPcNum++;
 		}
 
 		// modelにイベントの一覧をセット
@@ -64,11 +71,25 @@ public class EquipController {
 	 * 機器詳細を表示します.
 	 * @param model
 	 * @return 機器一覧画面のパス
+	 * @throws Exception
 	 */
 	@GetMapping("/equip/{id}")
-	public String equipDetail(@PathVariable("id") Integer id,Model model) {
+	public String equipDetail(@PathVariable("id") Integer id,Model model) throws Exception {
 		// DBに登録されている機器の一覧を取得
 		EquipEntity equip = equipService.findOneById(id);
+
+		if(equip == null) throw new Exception("機器データが取得できませんでした。");
+
+		if(equip.getIsLent()) {
+			LendEntity lend = lendService.findOneByEquipId(id);
+			if(lend == null) throw new Exception("貸出データが取得できませんでした。");
+			else {
+				UserEntity user = userService.findOneById(lend.getUserId());
+				if(user == null) throw new Exception("ユーザーデータが取得できませんでした。");
+				model.addAttribute("user", user);
+			}
+		}
+
 		System.out.println(equip);
 
 		// modelにイベントの一覧をセット
@@ -96,7 +117,7 @@ public class EquipController {
 	 */
 	@PostMapping("/equip/add")
 	public String equipAdd(@Validated @ModelAttribute("equip") EquipForm equipForm, BindingResult bindingResult) {
-		if(bindingResult.hasErrors()) {
+		if (bindingResult.hasErrors()) {
 			System.out.println(equipForm);
 			return "redirect:/equip/regist";
 		}
@@ -114,8 +135,9 @@ public class EquipController {
 	 * @return 機器一覧画面のパス
 	 */
 	@PostMapping("/equip/{id}/update")
-	public String equipUpdate(@PathVariable("id") Integer id,@Validated @ModelAttribute("equip") EquipForm equipForm, BindingResult bindingResult) {
-		if(bindingResult.hasErrors()) {
+	public String equipUpdate(@PathVariable("id") Integer id, @Validated @ModelAttribute("equip") EquipForm equipForm,
+			BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
 			System.out.println(equipForm);
 			return "error.html";
 		}
@@ -134,7 +156,8 @@ public class EquipController {
 	 */
 	@GetMapping("/equip/{id}/delete")
 	public String equipDelete(@PathVariable("id") Integer id) throws Exception {
-		if(lendService.findOneByEquipId(id)!= null) throw new Exception("現在貸出中の機器です。");
+		if (lendService.findOneByEquipId(id) != null)
+			throw new Exception("現在貸出中の機器です。");
 		equipService.deleteById(id);
 		// 次に表示する画面のパス（htmlファイルの名称）を返却
 		return "redirect:/equip/list";
