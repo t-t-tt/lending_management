ee40c737b8bf4ae4abd40dd75f8306c9d509ea0f
tt-tt-c <tsuruga10415@gmail.com>
2021-06-18 17:42:38 +0900

    テストデータ

---
+++ src/test/java/taro/service/EquipServiceTest.java
@@ -0,0 +1,62 @@
+package taro.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import taro.entity.EquipEntity;
+
+/**
+ * EquipServiceクラスのテストクラスです.
+ */
+@SpringBootTest
+public class EquipServiceTest {
+
+	@Autowired
+	EquipService equip_tableEntryService;
+
+	// 単体テスト結果検証用
+	@Autowired
+	EquipService equipService;
+
+	// テストデータの準備
+	@Test
+	@Sql(statements = {
+			"DELETE FROM equip_table",
+			"ALTER TABLE equip_table auto_increment = 1",
+			"INSERT INTO equip_table(asset,capacity,has_graphic_board,inventory_date,is_broken,is_deleted,is_lent,lease_end,lease_start,maker,memory,os,registration_date,remarks,storage_location,update_date) VALUES ('A19-06-001', '500GB', 0, '2021-03-01',0, 0,0, '2024-06-02', '2019-06-01', 'Dell', '8GB', 'Windows10','2021-03-01', '', 'classroom-001', '2021-03-01')",
+			"INSERT INTO equip_table(asset,capacity,has_graphic_board,inventory_date,is_broken,is_deleted,is_lent,lease_end,lease_start,maker,memory,os,registration_date,remarks,storage_location,update_date) VALUES ('A19-06-002', '500GB', 0, '2021-03-01',0, 0,0, '2024-06-02', '2019-06-01', 'Dell', '8GB', 'Windows10','2021-03-01', '', 'classroom-002', '2021-03-01')",
+			"INSERT INTO equip_table(asset,capacity,has_graphic_board,inventory_date,is_broken,is_deleted,is_lent,lease_end,lease_start,maker,memory,os,registration_date,remarks,storage_location,update_date) VALUES ('A19-06-003', '500GB', 0, '2021-03-01',0, 0,0, '2024-06-02', '2019-06-01', 'Dell', '8GB', 'Windows10','2021-03-01', '', 'classroom-003', '2021-03-01')",
+			"INSERT INTO equip_table(asset,capacity,has_graphic_board,inventory_date,is_broken,is_deleted,is_lent,lease_end,lease_start,maker,memory,os,registration_date,remarks,storage_location,update_date) VALUES ('A19-06-004', '500GB', 0, '2021-03-01',0, 0,0, '2024-06-02', '2019-06-01', 'Dell', '8GB', 'Windows10','2021-03-01', '', 'classroom-004', '2021-03-01')",
+			"INSERT INTO equip_table(asset,capacity,has_graphic_board,inventory_date,is_broken,is_deleted,is_lent,lease_end,lease_start,maker,memory,os,registration_date,remarks,storage_location,update_date) VALUES ('A19-06-005', '500GB', 0, '2021-03-01',0, 0,0, '2024-06-02', '2019-06-01', 'Dell', '8GB', 'Windows10','2021-03-01', '', 'classroom-005', '2021-03-01')",
+			"INSERT INTO equip_table(asset,capacity,has_graphic_board,inventory_date,is_broken,is_deleted,is_lent,lease_end,lease_start,maker,memory,os,registration_date,remarks,storage_location,update_date) VALUES ('A19-06-006', '500GB', 0, '2021-03-01',0, 0,0, '2024-06-02', '2019-06-01', 'Dell', '8GB', 'Windows10','2021-03-01', '', 'classroom-006', '2021-03-01')",
+			"INSERT INTO equip_table(asset,capacity,has_graphic_board,inventory_date,is_broken,is_deleted,is_lent,lease_end,lease_start,maker,memory,os,registration_date,remarks,storage_location,update_date) VALUES ('A19-06-007', '500GB', 0, '2021-04-02',0, 0,0, '2024-06-02', '2019-06-01', 'Dell', '8GB', 'Windows10','2021-03-01', '', 'classroom-007', '2021-03-01')",
+			"INSERT INTO equip_table(asset,capacity,has_graphic_board,inventory_date,is_broken,is_deleted,is_lent,lease_end,lease_start,maker,memory,os,registration_date,remarks,storage_location,update_date) VALUES ('A19-06-008', '500GB', 0, '2021-03-01',0, 0,0, '2024-06-02', '2019-06-01', 'Dell', '8GB', 'Windows10','2021-03-01', '', 'classroom-008', '2021-03-01')",
+			"INSERT INTO equip_table(asset,capacity,has_graphic_board,inventory_date,is_broken,is_deleted,is_lent,lease_end,lease_start,maker,memory,os,registration_date,remarks,storage_location,update_date) VALUES ('A19-06-009', '500GB', 0, '2021-03-01',0, 0,0, '2024-06-02', '2019-06-01', 'Dell', '8GB', 'Windows10','2021-03-01', '', 'classroom-009', '2021-03-01')",
+			"INSERT INTO equip_table(asset,capacity,has_graphic_board,inventory_date,is_broken,is_deleted,is_lent,lease_end,lease_start,maker,memory,os,registration_date,remarks,storage_location,update_date) VALUES ('A19-06-010', '500GB', 0, '2021-03-01',1, 0,0, '2024-06-02', '2019-06-01', 'Dell', '8GB', 'Windows10','2021-03-01', '', 'classroom-010', '2021-03-01')",
+			"INSERT INTO equip_table(asset,capacity,has_graphic_board,inventory_date,is_broken,is_deleted,is_lent,lease_end,lease_start,maker,memory,os,registration_date,remarks,storage_location,update_date) VALUES ('A19-06-011', '500GB', 0, '2021-03-01',0, 0,0, '2024-06-02', '2019-06-01', 'Dell', '8GB', 'Windows10','2021-03-01', '', 'classroom-011', '2021-03-01')",
+			"INSERT INTO equip_table(asset,capacity,has_graphic_board,inventory_date,is_broken,is_deleted,is_lent,lease_end,lease_start,maker,memory,os,registration_date,remarks,storage_location,update_date) VALUES ('A19-06-012', '500GB', 0, '2021-03-01',0, 0,0, '2024-06-02', '2019-06-01', 'Dell', '8GB', 'Windows10','2021-03-01', '', 'classroom-012', '2021-03-01')",
+			"INSERT INTO equip_table(asset,capacity,has_graphic_board,inventory_date,is_broken,is_deleted,is_lent,lease_end,lease_start,maker,memory,os,registration_date,remarks,storage_location,update_date) VALUES ('A19-06-013', '500GB', 0, '2021-03-01',0, 0,0, '2024-06-02', '2019-06-01', 'Dell', '8GB', 'Windows10','2021-03-01', '', 'classroom-013', '2021-03-01')",
+			"INSERT INTO equip_table(asset,capacity,has_graphic_board,inventory_date,is_broken,is_deleted,is_lent,lease_end,lease_start,maker,memory,os,registration_date,remarks,storage_location,update_date) VALUES ('A19-06-014', '500GB', 0, '2021-03-01',0, 0,0, '2024-06-02', '2019-06-01', 'Dell', '8GB', 'Windows10','2021-03-01', '', 'classroom-014', '2021-03-01')",
+			"INSERT INTO equip_table(asset,capacity,has_graphic_board,inventory_date,is_broken,is_deleted,is_lent,lease_end,lease_start,maker,memory,os,registration_date,remarks,storage_location,update_date) VALUES ('A19-06-015', '500GB', 0, '2021-03-01',0, 0,0, '2024-06-02', '2019-06-01', 'Dell', '8GB', 'Windows10','2021-03-01', '', 'classroom-015', '2021-03-01')",
+			"INSERT INTO equip_table(asset,capacity,has_graphic_board,inventory_date,is_broken,is_deleted,is_lent,lease_end,lease_start,maker,memory,os,registration_date,remarks,storage_location,update_date) VALUES ('A19-06-016', '500GB', 0, '2021-01-02',0, 0,0, '2024-06-02', '2019-06-01', 'Dell', '8GB', 'Windows10','2021-03-01', '', 'classroom-016', '2021-03-01')",
+			"INSERT INTO equip_table(asset,capacity,has_graphic_board,inventory_date,is_broken,is_deleted,is_lent,lease_end,lease_start,maker,memory,os,registration_date,remarks,storage_location,update_date) VALUES ('A19-06-017', '500GB', 0, '2021-01-03',0, 0,0, '2024-06-02', '2019-06-01', 'Dell', '8GB', 'Windows10','2021-03-01', '', 'classroom-017', '2021-03-01')",
+			"INSERT INTO equip_table(asset,capacity,has_graphic_board,inventory_date,is_broken,is_deleted,is_lent,lease_end,lease_start,maker,memory,os,registration_date,remarks,storage_location,update_date) VALUES ('A19-06-018', '500GB', 0, '2021-01-04',0, 0,0, '2024-06-02', '2019-06-01', 'Dell', '8GB', 'Windows10','2021-03-01', '', 'classroom-018', '2021-03-01')",
+			"INSERT INTO equip_table(asset,capacity,has_graphic_board,inventory_date,is_broken,is_deleted,is_lent,lease_end,lease_start,maker,memory,os,registration_date,remarks,storage_location,update_date) VALUES ('A20-01-001', '500GB', 0, '2021-03-01',0, 0,0, '2025-01-21', '2020-01-20', 'HP', '16GB', 'Windows10','2021-03-01', '共有フォルダ未設定', 'classroom-019', '2021-03-01')",
+			"INSERT INTO equip_table(asset,capacity,has_graphic_board,inventory_date,is_broken,is_deleted,is_lent,lease_end,lease_start,maker,memory,os,registration_date,remarks,storage_location,update_date) VALUES ('A20-01-002', '500GB', 0, '2021-03-01',0, 0,0, '2025-01-21', '2020-01-20', 'HP', '16GB', 'Windows10','2021-03-01', '共有フォルダ未設定', 'classroom-020', '2021-03-01')",
+			"INSERT INTO equip_table(asset,capacity,has_graphic_board,inventory_date,is_broken,is_deleted,is_lent,lease_end,lease_start,maker,memory,os,registration_date,remarks,storage_location,update_date) VALUES ('A20-01-003', '500GB', 0, '2021-03-01',0, 0,0, '2025-01-21', '2020-01-20', 'HP', '16GB', 'Windows10','2021-03-01', '共有フォルダ未設定', 'classroom-021', '2021-03-01')",
+			"INSERT INTO equip_table(asset,capacity,has_graphic_board,inventory_date,is_broken,is_deleted,is_lent,lease_end,lease_start,maker,memory,os,registration_date,remarks,storage_location,update_date) VALUES ('A20-01-004', '500GB', 0, '2021-03-01',0, 0,0, '2025-01-21', '2020-01-20', 'HP', '16GB', 'Windows10','2021-03-01', '共有フォルダ未設定', 'classroom-022', '2021-03-01')",
+			"INSERT INTO equip_table(asset,capacity,has_graphic_board,inventory_date,is_broken,is_deleted,is_lent,lease_end,lease_start,maker,memory,os,registration_date,remarks,storage_location,update_date) VALUES ('A20-01-005', '500GB', 0, '2021-03-01',0, 0,0, '2025-01-21', '2020-01-20', 'HP', '16GB', 'Windows10','2021-03-01', '共有フォルダ未設定', 'classroom-023', '2021-03-01')",
+			"INSERT INTO equip_table(asset,capacity,has_graphic_board,inventory_date,is_broken,is_deleted,is_lent,lease_end,lease_start,maker,memory,os,registration_date,remarks,storage_location,update_date) VALUES ('A20-01-006', '500GB', 0, '2021-03-01',0, 0,0, '2025-01-21', '2020-01-20', 'HP', '16GB', 'Windows10','2021-03-01', '共有フォルダ未設定', 'classroom-024', '2021-03-01')",
+			"INSERT INTO equip_table(asset,capacity,has_graphic_board,inventory_date,is_broken,is_deleted,is_lent,lease_end,lease_start,maker,memory,os,registration_date,remarks,storage_location,update_date) VALUES ('A20-01-007', '500GB', 0, '2021-03-01',0, 0,0, '2025-01-21', '2020-01-20', 'HP', '16GB', 'Windows10','2021-03-01', '共有フォルダ未設定', 'classroom-025', '2021-03-01')",
+			"INSERT INTO equip_table(asset,capacity,has_graphic_board,inventory_date,is_broken,is_deleted,is_lent,lease_end,lease_start,maker,memory,os,registration_date,remarks,storage_location,update_date) VALUES ('B20-01-001', '1TB', 1, '2021-06-01',0, 0,0, '2025-02-01', '2020-01-31', 'Mouse', '32GB', 'Windows10','2021-03-01', 'VR用　グラボ8GB', 'classroom-026', '2021-03-01')",
+			"INSERT INTO equip_table(asset,capacity,has_graphic_board,inventory_date,is_broken,is_deleted,is_lent,lease_end,lease_start,maker,memory,os,registration_date,remarks,storage_location,update_date) VALUES ('B20-01-002', '1TB', 1, '2021-06-01',0, 0,0, '2025-02-01', '2020-01-31', 'Mouse', '32GB', 'Windows10','2021-03-01', 'VR用　グラボ8GB', 'classroom-027', '2021-03-01')",
+			"INSERT INTO equip_table(asset,capacity,has_graphic_board,inventory_date,is_broken,is_deleted,is_lent,lease_end,lease_start,maker,memory,os,registration_date,remarks,storage_location,update_date) VALUES ('B20-01-003', '1TB', 1, '2021-06-01',0, 0,0, '2025-02-01', '2020-01-31', 'Mouse', '32GB', 'Windows10','2021-03-01', 'VR用　グラボ8GB', 'classroom-028', '2021-03-01')",
+
+	})
+	public void generateEquipTestData() {
+		EquipEntity equip = equipService.findOneById(1);
+		System.out.println(equip);
+	}
+}
---
+++ src/test/java/taro/service/UserServiceTest.java
@@ -0,0 +1,46 @@
+package taro.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import taro.entity.UserEntity;
+
+/**
+ * UserServiceクラスのテストクラスです.
+ */
+@SpringBootTest
+public class UserServiceTest {
+
+	@Autowired
+	UserService user_tableEntryService;
+
+	// 単体テスト結果検証用
+	@Autowired
+	UserService userService;
+
+	// テストデータの準備
+	@Test
+	@Sql(statements = {
+			"DELETE FROM user_table",
+			"ALTER TABLE user_table auto_increment = 1",
+			"INSERT INTO user_table(age,department,employee_number,gender,is_deleted,mail,name,name_kana,position,privilege,registration_date,retirement_date,tel_number,update_date) VALUES (49, '開発1課', 'A0001', '女', 0, 'katsumi124@clpxtxlcu.ff','森脇克美', 'モリワキカツミ', '一般', '利用者', '2021-03-01',null, 235868783, '2021-03-01')",
+			"INSERT INTO user_table(age,department,employee_number,gender,is_deleted,mail,name,name_kana,position,privilege,registration_date,retirement_date,tel_number,update_date) VALUES (55, '開発2課', 'A0002', '女', 0, 'amanabe@lkfolssvfl.hczpp.pc','真鍋沙奈', 'マナベサナ', '一般', '利用者', '2021-03-01',null, 264640388, '2021-03-01')",
+			"INSERT INTO user_table(age,department,employee_number,gender,is_deleted,mail,name,name_kana,position,privilege,registration_date,retirement_date,tel_number,update_date) VALUES (25, '開発2課', 'A0003', '女', 0, 'Aki_Takanashi@ajtkcn.yk','高梨愛姫', 'タカナシアキ', '一般', '利用者', '2021-03-01',null, 733344955, '2021-03-01')",
+			"INSERT INTO user_table(age,department,employee_number,gender,is_deleted,mail,name,name_kana,position,privilege,registration_date,retirement_date,tel_number,update_date) VALUES (46, '開発2課', 'A0004', '女', 0, 'ayaka53863@hnrwwzt.ouk','浅川彩香', 'アサカワアヤカ', '一般', '利用者', '2021-03-01',null, 733344944, '2021-03-01')",
+			"INSERT INTO user_table(age,department,employee_number,gender,is_deleted,mail,name,name_kana,position,privilege,registration_date,retirement_date,tel_number,update_date) VALUES (22, '開発2課', 'A0005', '女', 0, 'rinkayamashiro@iriqg.bkxcb.ttc','山城凛香', 'ヤマシロリンカ', '一般', '利用者', '2021-03-01',null, 733344333, '2021-03-01')",
+			"INSERT INTO user_table(age,department,employee_number,gender,is_deleted,mail,name,name_kana,position,privilege,registration_date,retirement_date,tel_number,update_date) VALUES (55, '開発1課', 'A0006', '女', 0, 'mizuki_ikuta@dkjnjxka.lran.sk','生田瑞紀', 'イクタミズキ', '一般', '利用者', '2021-03-01','2021/5/30', 733344222, '2021-03-01')",
+			"INSERT INTO user_table(age,department,employee_number,gender,is_deleted,mail,name,name_kana,position,privilege,registration_date,retirement_date,tel_number,update_date) VALUES (26, '開発1課', 'A0007', '男', 0, 'gl-fgujhlzunawvmineto16563@qdywk.nwned.fk','小峰嶺渡', 'コミネミネト', '一般', '利用者', '2021-03-01',null, 733344111, '2021-03-01')",
+			"INSERT INTO user_table(age,department,employee_number,gender,is_deleted,mail,name,name_kana,position,privilege,registration_date,retirement_date,tel_number,update_date) VALUES (50, '開発1課', 'A0008', '男', 0, 'shinichi117@htnsvbdwmx.df.hi','紺野信一', 'コンノシンイチ', '一般', '利用者', '2021-03-01',null, 733344819, '2021-03-01')",
+			"INSERT INTO user_table(age,department,employee_number,gender,is_deleted,mail,name,name_kana,position,privilege,registration_date,retirement_date,tel_number,update_date) VALUES (53, '開発1課', 'A0009', '男', 0, 'Masayoshi_Hatano@mwxuaibo.nxxo.cc','波多野正義', 'ハタノマサヨシ', '一般', '利用者', '2021-03-01',null, 733344351, '2021-03-01')",
+			"INSERT INTO user_table(age,department,employee_number,gender,is_deleted,mail,name,name_kana,position,privilege,registration_date,retirement_date,tel_number,update_date) VALUES (36, '開発1課', 'A0010', '女', 0, 'oyamaguchi@hfyc.jvj','山口桜子', 'ヤマグチサクラコ', '一般', '利用者', '2021-03-01',null, 733344968, '2021-03-01')",
+			"INSERT INTO user_table(age,department,employee_number,gender,is_deleted,mail,name,name_kana,position,privilege,registration_date,retirement_date,tel_number,update_date) VALUES (40, '開発1課', 'A0011', '女', 0, 'Keiko_Ban@ydtq.vi.qot','伴景子', 'バンケイコ', '部長', '管理者', '2021-03-01',null, 733344930, '2021-03-01')",
+
+
+	})
+	public void generateUserTestData() {
+		UserEntity user = userService.findOneById(1);
+		System.out.println(user);
+	}
+}
